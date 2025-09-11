0da240b64e145ced57b8e422d898223fbd30f7cd
Tex-LeTrongTin <trongtin23102006@gmail.com>
2025-09-12 06:43:02 +0700

    nopbaitap

---
+++ THtuan3_BaiTap2_LeTrongTin.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author ACER
+ */
+public class THtuan3_BaiTap2_LeTrongTin 
+{
+    public static void main(String args[]) 
+    {
+        String ten = "Le Trong Tin";
+        int tuoi = 18;
+        double chieucao = 1.78;
+        boolean thichltk = true;
+
+        System.out.println("--Thong tin ca nhan--");
+        System.out.println("Ten: " + ten + "");
+        System.out.println("Tuoi: " + tuoi + "");
+        System.out.println("Chieu cao: " + chieucao + " m");
+        System.out.println("Co thich lap trinh khong: " + thichltk + "");
+    }
+}
